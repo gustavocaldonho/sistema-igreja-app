79837c10abf494d3a43021cacc72b3dc97e6979c
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-04-20 16:45:08 -0300

    including mask in inputs

--- sistema-igreja-app/src/components/FormLogin/index.js
+++ sistema-igreja-app/src/components/FormLogin/index.js
@@ -1,11 +1,73 @@
 import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  TouchableOpacity,
+  Vibration,
+  Keyboard,
+} from "react-native";
 import styles from "./style";
 
+import MaskInput from "react-native-mask-input";
+
+const CPF_MASK = [
+  /\d/,
+  /\d/,
+  /\d/,
+  ".",
+  /\d/,
+  /\d/,
+  /\d/,
+  ".",
+  /\d/,
+  /\d/,
+  /\d/,
+  "-",
+  /\d/,
+  /\d/,
+];
+
 export default function FormLogin() {
+  const [cpf, setCpf] = useState("");
+
   return (
-    <View>
-      <Text>FormLogin</Text>
+    <View style={styles.formContext}>
+      <Pressable style={styles.form}>
+        <Text style={styles.formLabel}>CPF</Text>
+        <Text style={styles.errorMessage}></Text>
+        <MaskInput
+          style={styles.input}
+          placeholder="000.000.000-00"
+          keyboardType="numeric"
+          maxLength={14}
+          value={cpf}
+          onChangeText={setCpf}
+          mask={(text) => {
+            if (text.replace(/\D+/g, "").length <= 11) {
+              return CPF_MASK;
+            } else {
+            }
+          }}
+        ></MaskInput>
+
+        <Text style={styles.formLabel}>Senha</Text>
+        <Text style={styles.errorMessage}></Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          secureTextEntry={true}
+        ></TextInput>
+
+        <TouchableOpacity style={styles.buttonLogin}>
+          <Text style={styles.textButtonLogin}>Entrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonLogin}>
+          <Text style={styles.textButtonLogin}>Cadastrar</Text>
+        </TouchableOpacity>
+      </Pressable>
     </View>
   );
 }
