a9239691afec17d1d510ad6ec39281d5e4aa273f
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-05-03 09:35:40 -0300

    make InputgroupCpf

---
+++ my-app/src/components/FormLogin/InputGroupCpf/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import MaskInput from "react-native-mask-input";
+import styles from "./style";
+
+const CPF_MASK = [
+  /\d/,
+  /\d/,
+  /\d/,
+  ".",
+  /\d/,
+  /\d/,
+  /\d/,
+  ".",
+  /\d/,
+  /\d/,
+  /\d/,
+  "-",
+  /\d/,
+  /\d/,
+];
+
+const InputGroupCpf = ({ iconName, placeholder, value, onChangeText }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.boxIcon}>
+        <Icon name={iconName} size={20} color="#000" style={styles.icon} />
+      </View>
+      <MaskInput
+        style={styles.input}
+        placeholder={placeholder}
+        keyboardType="numeric"
+        maxLength={14}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor="#ccc"
+        mask={(text) => {
+          if (text.replace(/\D+/g, "").length <= 11) {
+            return CPF_MASK;
+          } else {
+          }
+        }}
+      />
+    </View>
+  );
+};
+
+export default InputGroupCpf;
--- my-app/src/components/FormLogin/index.js
+++ my-app/src/components/FormLogin/index.js
@@ -10,26 +10,11 @@ import {
   ScrollView,
   KeyboardAvoidingView,
 } from "react-native";
-import styles from "./style";
-
 import MaskInput from "react-native-mask-input";
 
-const CPF_MASK = [
-  /\d/,
-  /\d/,
-  /\d/,
-  ".",
-  /\d/,
-  /\d/,
-  /\d/,
-  ".",
-  /\d/,
-  /\d/,
-  /\d/,
-  "-",
-  /\d/,
-  /\d/,
-];
+import InputGroupCpf from "./InputGroupCpf";
+
+import styles from "./style";
 
 export default function FormLogin() {
   const [cpf, setCpf] = useState("");
@@ -38,22 +23,15 @@ export default function FormLogin() {
     // usar <ScrollView></ScrollView>
     <View style={styles.formContext}>
       <Pressable style={styles.form} onPress={Keyboard.dismiss}>
-        {/* <Text style={styles.formLabel}>CPF</Text> */}
-        <MaskInput
-          style={styles.input}
+        <InputGroupCpf
+          iconName="user"
           placeholder="000.000.000-00"
-          keyboardType="numeric"
-          maxLength={14}
           value={cpf}
           onChangeText={setCpf}
-          mask={(text) => {
-            if (text.replace(/\D+/g, "").length <= 11) {
-              return CPF_MASK;
-            } else {
-            }
-          }}
-        ></MaskInput>
-        <Text style={styles.errorMessage}>error message</Text>
+          style={styles.inputGroup}
+        />
+
+        <Text style={styles.errorMessage}></Text>
 
         {/* <Text style={styles.formLabel}>Senha</Text> */}
         <TextInput
@@ -61,7 +39,7 @@ export default function FormLogin() {
           placeholder="Digite sua senha"
           secureTextEntry={true}
         ></TextInput>
-        <Text style={styles.errorMessage}>error message</Text>
+        <Text style={styles.errorMessage}></Text>
 
         <TouchableOpacity style={styles.buttonLogin}>
           <Text style={styles.textButtonLogin}>Entrar</Text>
