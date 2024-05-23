bf88a883f955e09c48a330a3c575a837dc529589
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-05-23 11:49:43 -0300

    style gradiente caixa mortuaria

--- my-app/src/components/CaixaMortuaria/index.js
+++ my-app/src/components/CaixaMortuaria/index.js
@@ -9,6 +9,7 @@ import {
   Image,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
 import styles from "./style";
 import Icon from "react-native-vector-icons/FontAwesome";
 
@@ -20,31 +21,41 @@ export default function CaixaMortuaria({ navigation }) {
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#fff" />
-      <ScrollView style={styles.main}>
-        <View style={styles.boxItem}>
-          <View style={styles.boxTop}>
-            <View style={styles.boxStatus}>
-              <Text style={styles.textStatus}>Aguardando Pagamento</Text>
+      <LinearGradient
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 0, y: 0.1 }}
+        colors={["#f094c0", "#339dd7"]}
+      >
+        <ScrollView style={styles.main}>
+          <View style={styles.boxItem}>
+            <View style={styles.boxTop}>
+              <View style={styles.boxStatus}>
+                <Text style={styles.textStatus}>Aguardando Pagamento</Text>
+              </View>
+              <View style={styles.boxYear}>
+                <Text style={styles.textYear}>2024</Text>
+              </View>
             </View>
-            <View style={styles.boxYear}>
-              <Text style={styles.textYear}>2024</Text>
+            <View style={styles.boxMiddle}>
+              <Text style={styles.textValue}>R$ 50,00</Text>
+            </View>
+            <View style={styles.boxBottom}>
+              <View style={styles.boxObs}>
+                <Text style={styles.textObs}>Vencimento em 30/09/2024</Text>
+              </View>
+              <View style={styles.boxPayButton}>
+                <TouchableOpacity style={styles.payButton}>
+                  <Text style={styles.textPayButton}>Copiar Código</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-          <View style={styles.boxMiddle}>
-            <Text style={styles.textValue}>R$ 50,00</Text>
-          </View>
-          <View style={styles.boxBottom}>
-            <View style={styles.boxObs}>
-              <Text style={styles.textObs}>Vencimento em 30/09/2024</Text>
-            </View>
-            <View style={styles.boxPayButton}>
-              <TouchableOpacity style={styles.payButton}>
-                <Text style={styles.textPayButton}>Copiar Código</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </LinearGradient>
     </View>
   );
 }
