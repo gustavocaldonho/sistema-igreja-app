0b9b780e7fe6d16a5ee9419c50b25c94b16e1194
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-06-14 14:27:24 -0300

    style buttonback

--- my-app/src/components/ButtonBack/index.js
+++ my-app/src/components/ButtonBack/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { StyleSheet } from "react-native";
 
 export default function ButtonBack({ navigation, color }) {
   return (
     <TouchableOpacity onPress={() => navigation.goBack()}>
-      <Icon
-        name="chevron-left"
-        size={25}
-        color={color}
-        style={{ padding: 20 }}
-        // style={{ padding: 20, borderWidth: 1 }}
-      />
+      <Icon name="chevron-left" size={25} color={color} style={styles.icon} />
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    paddingVertical: 10,
+    paddingLeft: 20,
+  },
+});
