0286354b80413b8f9b337d441645125c1687b9cb
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-10-03 16:12:32 -0300

    comentary

--- my-app/src/services/warning_api.js
+++ my-app/src/services/warning_api.js
@@ -4,9 +4,7 @@ export const getTenWarnings = async (community, token, page) => {
   const pageSize = 10;
   try {
     const response = await api.get(
-      // /community/warnings/{community_patron}/paginated/{page}/{page_size}
       `/community/warnings/${community}/paginated/${page}/${pageSize}`,
-      // { page, page_size: 10 },
       {
         headers: {
           "Content-Type": "application/json",
