e960556e51980a85c5d59a4a4c8395e92140bd29
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-12-26 19:26:21 -0300

    style boxFiltersAndMain teste 2

--- my-app/src/components/screens/Financeiro/style.js
+++ my-app/src/components/screens/Financeiro/style.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
   },
   boxFiltersAndMain: {
-    height: "81%", //##### deixar responsivo
+    height: "80%", //##### deixar responsivo
     backgroundColor: "#fff",
     borderRadius: 10,
     overflow: "hidden",
