ba5e1701492cc6414952ae260637183fe19eaf88
Gustavo Caldonho <gcaldonho3@gmail.com>
2025-03-21 14:11:59 -0300

    install packages chart-kit and react-native-svg

--- my-app/src/components/screens/Financeiro/ModalCharts/index.js
+++ my-app/src/components/screens/Financeiro/ModalCharts/index.js
@@ -6,13 +6,71 @@ import {
   TouchableOpacity,
   StatusBar,
   ScrollView,
+  Dimensions,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { styles } from "./style";
+import { PieChart } from "react-native-chart-kit";
+
+const screenWidth = Dimensions.get("window").width;
 
 export default function ModalCharts({ visible, onClose }) {
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2,
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false,
+  };
+
+  const data = [
+    {
+      name: "Seoul",
+      population: 21500000,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Toronto",
+      population: 2800000,
+      color: "#F00",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Beijing",
+      population: 527612,
+      color: "red",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "New York",
+      population: 8538000,
+      color: "#ffffff",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Moscow",
+      population: 11920000,
+      color: "rgb(0, 0, 255)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+  ];
+
   return (
-    <Modal visible={visible} animationType="slide" transparent={false}>
+    <Modal
+      visible={visible}
+      animationType="slide"
+      transparent={false}
+      presentationStyle="fullScreen"
+    >
       <StatusBar
         translucent
         barStyle="light-content"
@@ -26,7 +84,19 @@ export default function ModalCharts({ visible, onClose }) {
           <Text style={styles.title}>Gráficos</Text>
         </View>
 
-        <ScrollView style={styles.content}></ScrollView>
+        <ScrollView style={styles.content}>
+          <PieChart
+            data={data}
+            width={screenWidth - 20}
+            height={220}
+            chartConfig={chartConfig}
+            accessor="population"
+            backgroundColor="transparent"
+            paddingLeft={15}
+            center={[10, 50]}
+            absolute
+          />
+        </ScrollView>
       </View>
     </Modal>
   );
